3bdfc31e18b3231425a577452b69590e5f2aa43d
Tom Paavolainen <tuomas.paavolainen@metropolia.fi>
2020-03-05 11:08:26 +0200

    augmented images

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,39 +1,23 @@
 package com.example.myapplication
 
-import android.Manifest
-import android.content.Intent
-import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
-import android.view.MotionEvent
-import android.view.SurfaceHolder
-import android.view.SurfaceView
 import android.view.View
-import android.widget.*
-import androidx.appcompat.app.AlertDialog
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-import com.google.android.gms.vision.CameraSource
-import com.google.android.gms.vision.Detector
-import com.google.android.gms.vision.barcode.Barcode
-import com.google.android.gms.vision.barcode.BarcodeDetector
+import com.google.ar.core.*
+import com.google.ar.sceneform.AnchorNode
+import com.google.ar.sceneform.rendering.ModelRenderable
+import com.google.ar.sceneform.ux.ArFragment
+import com.google.ar.sceneform.ux.TransformableNode
 import com.google.gson.GsonBuilder
 import okhttp3.*
 import okio.IOException
-import java.net.URL
-import com.google.ar.core.Anchor
-import com.google.ar.core.HitResult
-import com.google.ar.core.Plane
-import com.google.ar.sceneform.AnchorNode
-import com.google.ar.sceneform.rendering.Renderable
-import com.google.ar.sceneform.rendering.ViewRenderable
-import com.google.ar.sceneform.ux.ArFragment
-import com.google.ar.sceneform.ux.TransformableNode
-import kotlinx.android.synthetic.main.ar_fragment.*
 
 
-    data class Products(val data: List<Product>)
+data class Products(val data: List<Product>)
     data class Product(
         val id: Int,
         val name: String,
@@ -46,101 +30,73 @@ import kotlinx.android.synthetic.main.ar_fragment.*
 
 class MainActivity : AppCompatActivity() {
 
-    var arFragment: ArFragment? = null
-    var isFragmentLoaded = false
-    val fragManager = supportFragmentManager
-
-
-    private lateinit var svBarcode: SurfaceView
-    private lateinit var tvBarcode: TextView
-
-    private lateinit var detector: BarcodeDetector
-    private lateinit var cameraSource: CameraSource
+    private lateinit var fragment: ArFragment
 
     private var fitToScanImageView: ImageView? = null
+    private lateinit var karhuRenderable: ModelRenderable
 
 
+    private lateinit var cameraSource: Camera
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        qrScanner()
-
+        fragment = supportFragmentManager.findFragmentById(R.id.arimage_fragment) as ArFragment
         fitToScanImageView = findViewById(R.id.fit_to_scan_img)
 
 
-        /** MARK: AR-creation
-        arFragment = sceneform_fragment as ArFragment
-        arFragment.setOnTapArPlaneListener { hitResult: HitResult, plane: Plane, motionEvent: MotionEvent ->
-            if (plane.type != Plane.Type.HORIZONTAL_UPWARD_FACING) {
-                return@setOnTapArPlaneListener
-            }
-            val anchor = hitResult.createAnchor()
-            placeObject(arFragment, anchor)
-        }*/
+
+        val karhu = ModelRenderable.builder()
+            .setSource(this, Uri.parse("igloo.sfb"))
+            .build()
+        karhu.thenAccept { it -> karhuRenderable = it }
+//
+//        val andy = ModelRenderable.builder()
+//            .setSource(this, Uri.parse("andy.sfb"))
+//            .build()
+//        andy.thenAccept {it -> andyRenderable = it }
+
+        fragment.arSceneView.scene.addOnUpdateListener { frameTime ->
+            frameUpdate()
+        }
     }
-
-    fun qrScanner() {
-
-        svBarcode = findViewById(R.id.svBarcode)
-        tvBarcode = findViewById(R.id.tvBarcode)
-
-        detector = BarcodeDetector.Builder(this).setBarcodeFormats(Barcode.QR_CODE).build()
-        detector.setProcessor(object : Detector.Processor<Barcode> {
-            override fun release() {}
-            override fun receiveDetections(detections: Detector.Detections<Barcode>?){
-                val barcodes = detections?.detectedItems
-                if (barcodes!!.size() > 0) {
-                    tvBarcode.post {
-                        val text = barcodes.valueAt(0).displayValue
-                        tvBarcode.text = text
-                        // put scanned text to toast
-                        Log.d("dbg", text)
-                        Toast.makeText(this@MainActivity, text, Toast.LENGTH_SHORT)
-                            .show()
-                        fetchJson()
-                        if (!isFragmentLoaded) {
-                            Log.d("dbg", "showfragment")
-                            showFragment()
-                            fitToScanImageView!!.visibility = View.GONE
-                        }
-
-
-
+    
+        private fun frameUpdate() {
+            val arFrame = fragment.arSceneView.arFrame
+            if (arFrame == null || arFrame.camera.trackingState != TrackingState.TRACKING) {
+                return
+            }
+            val updatedAugmentedImages = arFrame.getUpdatedTrackables(AugmentedImage::class.java)
+            updatedAugmentedImages.forEach {
+                when(it.trackingState) {
+                    TrackingState.PAUSED -> {
+                        val text = "Detected Image: " + it.name + " - need more info"
+                        Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
                     }
+                    TrackingState.TRACKING -> {
+                        Log.d("dbg", "hees")
+                        var anchors = it.anchors
+                        if( anchors.isEmpty() ) {
+                            fitToScanImageView?.visibility = View.GONE
+                            val pose = it.centerPose
+                            val anchor = it.createAnchor(pose)
+                            val anchorNode = AnchorNode(anchor)
+                            anchorNode.setParent(fragment.arSceneView.scene)
+                            val imgNode = TransformableNode(fragment.transformationSystem)
+                            imgNode.setParent(anchorNode)
+                            if (it.name == "color") {
+                                imgNode.renderable = karhuRenderable
+                            }
+                        }
+                    }
+                        TrackingState.STOPPED -> {
+                    val text = "Tracking stopped: " + it.name
+                    Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
+                }
                 }
             }
-        })
+        }
 
-        cameraSource = CameraSource.Builder(this, detector).setRequestedPreviewSize(1024, 768)
-            .setRequestedFps(25f).setAutoFocusEnabled(true).build()
-
-        svBarcode.holder.addCallback(object : SurfaceHolder.Callback2 {
-            override fun surfaceRedrawNeeded(holder: SurfaceHolder?) {
-
-            }
-
-            override fun surfaceChanged(
-                holder: SurfaceHolder?,
-                format: Int,
-                width: Int,
-                height: Int
-            ) {
-
-            }
-
-            override fun surfaceDestroyed(holder: SurfaceHolder?) {
-                cameraSource.stop()
-            }
-
-            override fun surfaceCreated(holder: SurfaceHolder?) {
-                if (ContextCompat.checkSelfPermission(this@MainActivity,
-                        Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED)
-                    cameraSource.start(holder)
-                else ActivityCompat.requestPermissions(this@MainActivity,
-                    arrayOf(Manifest.permission.CAMERA), 123)
-            }
-        })
-    }
 
     private fun fetchJson() {
         val url = "http://users.metropolia.fi/~tuomamp/arData.json"
@@ -168,92 +124,6 @@ class MainActivity : AppCompatActivity() {
             }
         })
     }
-
-    private fun placeObject(fragment: ArFragment, anchor: Anchor) {
-        ViewRenderable.builder()
-            .setView(fragment.context, R.layout.price_tag)
-            .build()
-            .thenAccept {
-                it.isShadowCaster = false
-                it.isShadowReceiver = false
-                addControlsToScene(fragment, anchor, it)
-            }
-            .exceptionally {
-                val builder = AlertDialog.Builder(this)
-                builder.setMessage(it.message).setTitle("Error")
-                val dialog = builder.create()
-                dialog.show()
-                return@exceptionally null
-            }
-    }
-
-
-    fun trackPanes() {
-        Log.d("dbg", "trackpanes()")
-            Log.d("dbg", "in thread")
-        Thread {
-                arFragment = sceneform_fragment as? ArFragment
-                Log.d("dbg", "arfrag")
-                arFragment?.setOnTapArPlaneListener { hitResult: HitResult, plane: Plane, motionEvent: MotionEvent ->
-                    if (plane.type != Plane.Type.HORIZONTAL_UPWARD_FACING) {
-                        Log.d("dbg", "horizontal")
-                        return@setOnTapArPlaneListener
-                    }
-                    Log.d("dbg", "tapped")
-                    val anchor = hitResult.createAnchor()
-                    Log.d("dbg", "anchor created")
-                    placeObject(arFragment!!, anchor)
-                    Log.d("dbg", "object set")
-                }
-                }.start()
-    }
-
-    fun showFragment() {
-        Log.d("dbg", "showfragment() started")
-        val transaction = fragManager.beginTransaction()
-        Log.d("dbg", "begintransaction() ok")
-        val fragment = Fragment_AR()
-
-        transaction.replace(R.id.fragment_holder,fragment)
-        Log.d("dbg", "replace ok")
-        transaction.addToBackStack(null)
-        Log.d("dbg", "before commit")
-        transaction.commit()
-        Log.d("dbg", "commit ok")
-        isFragmentLoaded = true
-        trackPanes()
-
-    }
-
-    private fun addControlsToScene(fragment: ArFragment, anchor: Anchor, renderable: Renderable) {
-        val anchorNode = AnchorNode(anchor)
-        val node = TransformableNode(fragment.transformationSystem)
-        node.renderable = renderable
-        node.setParent(anchorNode)
-        fragment.arSceneView.scene.addChild(anchorNode)
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == 123) {
-            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)
-                cameraSource.start(svBarcode.holder)
-            else Toast.makeText(this, "Scanner won't work without permission", Toast.LENGTH_SHORT)
-                .show()
-        }
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        detector.release()
-        cameraSource.stop()
-        cameraSource.release()
-    }
-
 
 
 }
